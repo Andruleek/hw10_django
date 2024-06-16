a88432d65f1c6911afc25760d4f94c657b2594ed
ExtymAndriy <150171957+ExtymAndriy@users.noreply.github.com>
2024-06-16 17:57:02 +0300

    10

---
+++ hw_project/quotes/views.py
@@ -0,0 +1,13 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .utils import get_mongodb
+
+# Create your views here.
+
+def main(request, page=1):
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page)
+    quotes_on_page = paginator.page(page)
+    return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
